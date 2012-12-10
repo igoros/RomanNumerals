9b56842f5afad98a21c5b0e02629d108594987ae
igoros <igoros@Bunny-37.ACADEMY.JCE.AC.IL>
2012-12-10 12:44:37 +0200

    green

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -6,8 +6,8 @@ public class RomanNumerals {
 		if(nmber<1|| nmber>3999)
 			return "-1";
 		
-		int[] aArray={10,9,5,4,1};
-		String[] sArray={"X","IX","V","IV","I"};
+		int[] aArray={50,40,10,9,5,4,1};
+		String[] sArray={"L","XL","X","IX","V","IV","I"};
 		String number="";
 		
 		for(int i=0;i<aArray.length;i++)
