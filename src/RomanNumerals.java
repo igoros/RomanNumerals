e0c752714e3d15db0440a9c2ee3826279838e7c5
igoros <igoros@Bunny-37.ACADEMY.JCE.AC.IL>
2012-12-10 12:42:03 +0200

    green

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -3,10 +3,20 @@ public class RomanNumerals {
 
 	public String toRoman(int nmber)
 	{
+		if(nmber<1|| nmber>3999)
+			return "-1";
+		
+		int[] aArray={10,9,5,4,1};
+		String[] sArray={"X","IX","V","IV","I"};
 		String number="";
-		for (int i=0;i<nmber;i++)
+		
+		for(int i=0;i<aArray.length;i++)
 		{
-			number+="I";
+			while(nmber>=aArray[i])
+			{
+				number+=sArray[i];
+				nmber-=aArray[i];
+			}
 		}
 		return number;
 	}
